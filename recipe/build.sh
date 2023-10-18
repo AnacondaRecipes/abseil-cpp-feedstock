72925b983b28b83248047b5d4462429f8bc7ef7b
Serhii Kupriienko <skupriienko@anaconda.com>
2023-10-18 10:43:22 +0300

    Rebuild with import libs on win, other modifications

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
+set -exuo pipefail
+
 mkdir -p build
 cd build
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	  -DCMAKE_PREFIX_PATH=${PREFIX} \
-	  -DCMAKE_INSTALL_LIBDIR=lib \
-	  -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=ON \
-      -GNinja \
-	  ..
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
+cmake -G Ninja \
+	${CMAKE_ARGS} \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_PREFIX_PATH=${PREFIX} \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_CXX_STANDARD=17 \
+	-DABSL_PROPAGATE_CXX_STD=ON \
+    ..
 
 ninja install
