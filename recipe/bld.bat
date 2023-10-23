55aca93e542f412c65a99696938adb8f8d1692bd
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2023-10-23 12:42:22 +0300

    abseil-cpp 20230802.0: Rebuild with import libs on win (#6)
    * Rebuild with import libs on win, other modifications
    
    * Add pkg-config to test/requires, add a test suite

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,20 @@
 mkdir build
 cd build
+if %ERRORLEVEL% neq 0 exit 1
+
 cmake -GNinja ^
-      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-	  -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-	  -DCMAKE_BUILD_TYPE=Release ^
-	  ..
+	-DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+	-DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+	-DCMAKE_BUILD_TYPE=Release ^
+	-DCMAKE_CXX_STANDARD=17 ^
+	-DBUILD_SHARED_LIBS=ON ^
+    -DABSL_PROPAGATE_CXX_STD=ON ^
+    !EXTRA_ARGS! ^
+	..
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build .
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --target install
+if %ERRORLEVEL% neq 0 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,33 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+echo "Building ${PKG_NAME}."
+
+# Isolate the build.
 mkdir -p build
-cd build
+cd build || exit 1
 
-cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-	  -DCMAKE_PREFIX_PATH=${PREFIX} \
-	  -DCMAKE_INSTALL_LIBDIR=lib \
-	  -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_SHARED_LIBS=ON \
-      -GNinja \
-	  ..
+if [[ "${target_platform}" == osx-* ]]; then
+    # See https://conda-forge.org/docs/maintainer/knowledge_base.html#newer-c-features-with-old-sdk
+    CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
 
-ninja install
+# Generate the build files.
+echo "Generating the build files..."
+cmake -G Ninja \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_PREFIX_PATH=${PREFIX} \
+	-DCMAKE_INSTALL_LIBDIR=lib \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_SHARED_LIBS=ON \
+	-DCMAKE_CXX_STANDARD=17 \
+	-DABSL_PROPAGATE_CXX_STD=ON \
+    ..
+
+ninja install || exit 1
+
+# Error free exit!
+echo "Error free exit!"
+exit 0
+
